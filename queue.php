84148c8c5fb862629866ce01db37916cde92d768
princessvincent <priscavincent2018@gmail.com>
2023-03-23 18:51:17 -0800

    updated

--- queue.php
+++ queue.php
@@ -2,11 +2,135 @@
 
 namespace App\queue;
 
+use Dotenv\Dotenv;
 use mysqli;
+use PHPMailer\PHPMailer\Exception;
+use PHPMailer\PHPMailer\PHPMailer;
+
+//load phpmailer
+// require 'vendor/phpmailer/src/PHPMailer.php';
+// require 'vendor/phpmailer/src/Exception.php';
+// require 'vendor/phpmailer/src/SMTP.php';
+//Load Composer's autoloader
+require 'vendor/autoload.php';
 
 /**
  * Undefined class
  */
+
+class Notification
+{
+
+    private $queue;
+    private $from_email;
+
+    public function __construct(queue $queue, $from_email)
+    {
+        $this->queue = $queue;
+        $this->from_email = $from_email;
+    }
+
+    public function send_email($to, $subject, $message)
+    {
+        $data = array(
+            'to_email' => $to,
+            'from_email' => $this->from_email,
+            'subject' => $subject,
+            'message' => $message,
+        );
+
+        // Create a new queue with the email data
+        //   $stmt = "INSERT INTO queues (to_email, from_email,) VALUES (?, NOW())";
+        if ($this->queue->create_queue(json_encode($data))) {
+            echo "Email notification added to the queue.\n";
+            return true;
+        } else {
+            echo "Error adding email notification to the queue.\n";
+            return false;
+        }
+    }
+
+    public function process_queue()
+    {
+        while (true) {
+            $next_queue = $this->queue->get_next_queue();
+            if (!$next_queue) {
+                sleep(5); // Wait for 5 seconds before checking again
+                continue;
+            }
+            $data = json_decode($next_queue['data'], true);
+            $to = $data['to_email'];
+            $subject = $data['subject'];
+            $message = $data['message'];
+            $headers = "From: " . $data['from_email'] . "\r\n" .
+                "Reply-To: " . $data['from_email'] . "\r\n";
+
+            // Create a new instance of PHPMailer
+            $mail = new PHPMailer(true);
+
+            // Set the mailer to use SMTP
+            $mail->isSMTP();
+
+            // Set the SMTP host name
+            $mail->Host = 'sandbox.smtp.mailtrap.io'; // Update with your SMTP server details
+
+            // Set the SMTP port number - 587 for authenticated TLS, a.k.a. RFC4409 SMTP submission
+            $mail->Port = 2525;
+            $mail->SMTPAuth = true;
+
+            // Set the encryption system to use - ssl (deprecated) or tls
+            $mail->SMTPSecure = 'tls';
+
+            // Set the SMTP authentication username
+            $mail->Username = 'f55d08baf4a54d'; // Update with your email address
+
+            // Set the SMTP authentication password
+            $mail->Password = '2022dc62cf8878'; // Update with your email password
+
+            // Set the "From" email address and name
+            $mail->setFrom('priscavincent2018@gmail.com', 'Your Name');
+
+            // Set the "To" email address
+            $mail->addAddress($to);
+
+            // Set the email subject
+            $mail->Subject = $subject;
+
+            // Set the email body
+            $mail->Body = $message;
+            try {
+                // Send the email
+                $mail->send();
+
+                // Email sent successfully, mark the queue as completed
+                $this->queue->mark_queue_completed($next_queue['id']);
+                echo "Email notification sent to " . $to . "\n";
+            } catch (Exception $e) {
+                // Email failed to send, mark the queue as failed
+                $this->queue->mark_queue_failed($next_queue['id']);
+                echo "Error sending email notification to " . $to . ": " . $mail->ErrorInfo . "\n";
+            }
+            sleep(1); // Wait for 1 second before processing the next queue
+        }
+
+    }
+}
+
+// Create a new queue object
+$queue = new Queue();
+
+// Create a new notification object
+$notification = new Notification($queue, 'priscavincent2018@gmail.com');
+
+// Send an email notification
+$to = 'bukasongeneral2017@gmail.com';
+$subject = 'Test Email Notification';
+$message = 'This is a test email notification.';
+$notification->send_email($to, $subject, $message);
+
+// Process the queue to send the email
+$notification->process_queue();
+
 class queue
 {
     //     private $db;
@@ -18,26 +142,49 @@ class queue
 
     public function __construct()
     {
-        $this->conn = new mysqli($this->host, $this->username, $this->password, $this->database);
-        if ($this->conn->connect_error) {
-            die("Connection failed: " . $this->conn->connect_error);
-        }
+        //   $this->conn = new mysqli($this->host, $this->username, $this->password, $this->database);
+        //   if ($this->conn->connect_error) {
+        //       die("Connection failed: " . $this->conn->connect_error);
+        //   }
+
+        $dotenv = Dotenv::createImmutable(__DIR__);
+        $dotenv->load();
+
+            // Use the environment variables to establish the database connection
+        $this->conn = new mysqli(
+            $_ENV['DB_HOST'],
+            $_ENV['DB_USERNAME'],
+            $_ENV['DB_PASSWORD'],
+            $_ENV['DB_DATABASE']
+        );
     }
 
     // Create a new queue
     public function create_queue($data)
     {
-        global $conn;
-        $sql = "INSERT INTO queues (data, created_at) VALUES ('prisca', NOW())";
-      //   print_r($sql);
-        if ($this->conn->query($sql) == true) {
-            // $queue_id = $this->conn->insert_id;
-            echo "Queue created with ID: ";
-            // return $queue_id;
+        $stmt = $this->conn->prepare("INSERT INTO queues (data, created_at) VALUES (?, NOW())");
+        $stmt->bind_param("s", $data);
+
+        if ($stmt->execute()) {
+            echo "Queue created with ID: " . $stmt->insert_id . "\n";
+            return true;
         } else {
-            echo "Error creating queue: " . $this->conn->error;
+            echo "Error creating queue: " . $this->conn->error . "\n";
             return false;
         }
+
+        //   global $conn;
+        //   $sql = "INSERT INTO queues (data, created_at) VALUES ($data, NOW())";
+        //   //   print_r($sql);
+        //   if ($this->conn->query($sql) == true) {
+        //       // $queue_id = $this->conn->insert_id;
+        //       echo "Queue created with ID: ";
+        //       // return $queue_id;
+        //   } else {
+        //       echo "Error creating queue: " . $this->conn->error;
+        //       return false;
+        //   }
+
     }
 
     // Get the next queue to dispatch
@@ -105,20 +252,12 @@ class queue
         $result = $this->conn->query($sql);
         if ($result->num_rows > 0) {
             while ($row = $result->fetch_assoc()) {
-               $this->mark_queue_failed($row['id']);
+                $this->mark_queue_failed($row['id']);
             }
         }
     }
 
-    
-
 }
-
-// Example usage:
-// $queue = new queue();
-// $queue->create_queue('data1');
-// $queue->create_queue('data2');
-// $queue->create_queue('data3');
 
 // Cron job example:
 while (true) {
@@ -135,4 +274,61 @@ while (true) {
     sleep(1); // Wait for 1 second before processing the next queue
 }
 
+// class Notification
+// {
 
+//     private $queue;
+
+//     public function __construct()
+//     {
+//         $this->queue = new queue();
+//     }
+
+//     public function send_notification($user_id, $message)
+//     {
+//         $data = array(
+//             'user_id' => $user_id,
+//             'message' => $message,
+//             'sent_at' => null,
+//         );
+
+//         $this->queue->create_queue(json_encode($data));
+//     }
+
+//     public function dispatch_notifications()
+//     {
+//         while (true) {
+//             $next_queue = $this->queue->get_next_queue();
+//             if (!$next_queue) {
+//                 sleep(5); // Wait for 5 seconds before checking again
+//                 continue;
+//             }
+
+//             $data = json_decode($next_queue['data'], true);
+//             $user_id = $data['user_id'];
+//             $message = $data['message'];
+
+//             // Send the notification here
+//             $this->send_email_notification($user_id, $message);
+
+//             $this->queue->mark_queue_dispatched($next_queue['id']);
+//             $this->queue->mark_queue_completed($next_queue['id']);
+
+//             sleep(1); // Wait for 1 second before processing the next queue
+//         }
+//     }
+
+//     private function send_email_notification($user_id, $message)
+//     {
+//         // Code to send email notification to user
+//         // You can replace this with your own code to send notifications through your preferred channel
+//         echo "Sending notification to user $user_id: $message\n";
+//     }
+// }
+
+// // Example usage:
+// $notification = new Notification();
+// $notification->send_notification(1, "Hello, World!");
+// $notification->send_notification(2, "This is a test notification.");
+
+// // Cron
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -12,12 +12,14 @@ class ComposerStaticInit6276b82ee9779c32a1e4ee32db8e0cf6
         '0e6d7bf4a5811bfa5cf40c5ccd6fae6a' => __DIR__ . '/..' . '/symfony/polyfill-mbstring/bootstrap.php',
         'e39a8b23c42d4e1452234d762b03835a' => __DIR__ . '/..' . '/ramsey/uuid/src/functions.php',
         '6e3fae29631ef280660b3cdad06f25a8' => __DIR__ . '/..' . '/symfony/deprecation-contracts/function.php',
+        'a4a119a56e50fbb293281d9a48007e0e' => __DIR__ . '/..' . '/symfony/polyfill-php80/bootstrap.php',
     );
 
     public static $prefixLengthsPsr4 = array (
         'S' => 
         array (
             'Symfony\\Polyfill\\Php81\\' => 23,
+            'Symfony\\Polyfill\\Php80\\' => 23,
             'Symfony\\Polyfill\\Mbstring\\' => 26,
             'Symfony\\Polyfill\\Ctype\\' => 23,
             'Symfony\\Component\\Filesystem\\' => 29,
@@ -32,11 +34,17 @@ class ComposerStaticInit6276b82ee9779c32a1e4ee32db8e0cf6
         array (
             'Psr\\Log\\' => 8,
             'Psr\\Container\\' => 14,
+            'PhpOption\\' => 10,
+            'PHPMailer\\PHPMailer\\' => 20,
         ),
         'I' => 
         array (
             'Interop\\Queue\\' => 14,
             'Interop\\Amqp\\' => 13,
+        ),
+        'G' => 
+        array (
+            'GrahamCampbell\\ResultType\\' => 26,
         ),
         'E' => 
         array (
@@ -45,6 +53,10 @@ class ComposerStaticInit6276b82ee9779c32a1e4ee32db8e0cf6
             'Enqueue\\Fs\\' => 11,
             'Enqueue\\Dsn\\' => 12,
             'Enqueue\\' => 8,
+        ),
+        'D' => 
+        array (
+            'Dotenv\\' => 7,
         ),
         'B' => 
         array (
@@ -56,6 +68,10 @@ class ComposerStaticInit6276b82ee9779c32a1e4ee32db8e0cf6
         'Symfony\\Polyfill\\Php81\\' => 
         array (
             0 => __DIR__ . '/..' . '/symfony/polyfill-php81',
+        ),
+        'Symfony\\Polyfill\\Php80\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/symfony/polyfill-php80',
         ),
         'Symfony\\Polyfill\\Mbstring\\' => 
         array (
@@ -89,6 +105,14 @@ class ComposerStaticInit6276b82ee9779c32a1e4ee32db8e0cf6
         array (
             0 => __DIR__ . '/..' . '/psr/container/src',
         ),
+        'PhpOption\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/phpoption/phpoption/src/PhpOption',
+        ),
+        'PHPMailer\\PHPMailer\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/phpmailer/phpmailer/src',
+        ),
         'Interop\\Queue\\' => 
         array (
             0 => __DIR__ . '/..' . '/queue-interop/queue-interop/src',
@@ -96,6 +120,10 @@ class ComposerStaticInit6276b82ee9779c32a1e4ee32db8e0cf6
         'Interop\\Amqp\\' => 
         array (
             0 => __DIR__ . '/..' . '/queue-interop/amqp-interop/src',
+        ),
+        'GrahamCampbell\\ResultType\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/graham-campbell/result-type/src',
         ),
         'Enqueue\\SimpleClient\\' => 
         array (
@@ -117,6 +145,10 @@ class ComposerStaticInit6276b82ee9779c32a1e4ee32db8e0cf6
         array (
             0 => __DIR__ . '/..' . '/enqueue/enqueue',
         ),
+        'Dotenv\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/vlucas/phpdotenv/src',
+        ),
         'Brick\\Math\\' => 
         array (
             0 => __DIR__ . '/..' . '/brick/math/src',
@@ -134,8 +166,13 @@ class ComposerStaticInit6276b82ee9779c32a1e4ee32db8e0cf6
     );
 
     public static $classMap = array (
+        'Attribute' => __DIR__ . '/..' . '/symfony/polyfill-php80/Resources/stubs/Attribute.php',
         'Composer\\InstalledVersions' => __DIR__ . '/..' . '/composer/InstalledVersions.php',
+        'PhpToken' => __DIR__ . '/..' . '/symfony/polyfill-php80/Resources/stubs/PhpToken.php',
         'ReturnTypeWillChange' => __DIR__ . '/..' . '/symfony/polyfill-php81/Resources/stubs/ReturnTypeWillChange.php',
+        'Stringable' => __DIR__ . '/..' . '/symfony/polyfill-php80/Resources/stubs/Stringable.php',
+        'UnhandledMatchError' => __DIR__ . '/..' . '/symfony/polyfill-php80/Resources/stubs/UnhandledMatchError.php',
+        'ValueError' => __DIR__ . '/..' . '/symfony/polyfill-php80/Resources/stubs/ValueError.php',
     );
 
     public static function getInitializer(ClassLoader $loader)
